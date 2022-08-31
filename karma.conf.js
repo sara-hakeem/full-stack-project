f21707612f857cf8c86dc81171898ae9f36b9a28
sara-hakeem <44380562+sara-hakeem@users.noreply.github.com>
2022-08-31 03:01:12 +0200

    Add files via upload

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ashbal-alfan'),
+      dir: require('path').join(__dirname, './coverage/ashbal-alfan-admin'),
       subdir: '.',
       reporters: [
         { type: 'html' },
